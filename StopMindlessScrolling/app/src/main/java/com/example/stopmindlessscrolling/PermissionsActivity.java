250fbca8ad2e39df4c94e395859f134f43e98827
peyyalaamala <85278466+peyyalaamala@users.noreply.github.com>
2023-04-12 17:01:00 +0530

    display over other apps permission added

--- StopMindlessScrolling/app/src/main/java/com/example/stopmindlessscrolling/PermissionsActivity.java
+++ StopMindlessScrolling/app/src/main/java/com/example/stopmindlessscrolling/PermissionsActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -24,7 +25,7 @@ public class PermissionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usageAccessPermission();
+
 
     }
     /**
@@ -88,5 +89,61 @@ public class PermissionsActivity extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
+    }
+
+    private void displayoverotherApps(){
+        if (!Settings.canDrawOverlays(PermissionsActivity.this)) {
+
+            displayoverotherappsdialog();
+        }
+    }
+    /**
+     * To show usage access permission
+     */
+    public void displayoverotherappsdialog() {
+
+        Dialog dialog = new Dialog(PermissionsActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.usage_access_permission_popup);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setOnKeyListener(new Dialog.OnKeyListener() {
+
+            @Override
+            public boolean onKey(DialogInterface arg0, int keyCode,
+                                 KeyEvent event) {
+                // TODO Auto-generated method stub
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    dialog.dismiss();
+                }
+                return true;
+            }
+        });
+        TextView ok = (TextView) dialog.findViewById(R.id.okBtn);
+        TextView okTxt = (TextView) dialog.findViewById(R.id.okTxt);
+        okTxt.setText(R.string.display_over_apps);
+        dialog.show();
+        ok.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())));
+
+            }
+        });
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        usageAccessPermission();
+        displayoverotherApps();
     }
 }
